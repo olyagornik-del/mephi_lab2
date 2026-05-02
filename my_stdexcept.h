8c734e6c99f9194a804da397e69fbf385c2b1061
olyagornik-del <olya.gornik@gmail.com>
2026-05-02 12:38:00 +0300

    rewriting errors and deleting unnecessary eroros

--- my_stdexcept.h
+++ my_stdexcept.h
@@ -4,75 +4,33 @@
 #include <exception>
 #include <string>
 
-//класс logic_error -
-class LogicError : public std::exception {
-protected:
+class ParentForErrors: public std::exception {
+private:
     std::string message;
-public:
-    explicit LogicError(const std::string &msg) : message(msg) {}
-    explicit LogicError(const char *msg) : message(msg) {}
 
+protected:
+    explicit ParentForErrors(const char *msg): message(msg) {}
+
+public:
     const char* what() const noexcept override {
         return message.c_str();
     }
 };
+//правило ягни - вам никогда не понадобиться))
 
-//Производные от logic_error -
-class DomainError : public LogicError {
+class InvalidArgument : public ParentForErrors {
 public:
-    explicit DomainError(const std::string& msg) : LogicError(msg) {}
-    explicit DomainError(const char* msg) : LogicError(msg) {}
+    explicit InvalidArgument(const char* NameOfArgument) :
+    ParentForErrors((std::string("неправильный аргумент:") + NameOfArgument).c_str()) {}
+
+class OutOfRange : public ParentForErrors {
+public:
+    OutOfRange(const char* name, int value, int min, int max)
+        : ParentForErrors(
+            (std::string("значение ") + name + " = " + std::to_string(value) +
+             " вне диапазона [" + std::to_string(min) + ", " + std::to_string(max) + "]").c_str()) {}
 };
 
-class InvalidArgument : public LogicError {
-public:
-    explicit InvalidArgument(const std::string& msg) : LogicError(msg) {}
-    explicit InvalidArgument(const char* msg) : LogicError(msg) {}
-};
-
-class LengthError : public LogicError {
-public:
-    explicit LengthError(const std::string& msg) : LogicError(msg) {}
-    explicit LengthError(const char* msg) : LogicError(msg) {}
-};
-
-class OutOfRange : public LogicError {
-public:
-    explicit OutOfRange(const std::string& msg) : LogicError(msg) {}
-    explicit OutOfRange(const char* msg) : LogicError(msg) {}
-};
-
-//класс runtime_error -
-class RuntimeError : public std::exception {
-protected:
-    std::string message;
-public:
-    explicit RuntimeError(const std::string& msg) : message(msg) {}
-    explicit RuntimeError(const char* msg) : message(msg) {}
-
-    const char* what() const noexcept override {
-        return message.c_str();
-    }
-};
-
-//Производные от runtime_error ---
-class RangeError : public RuntimeError {
-public:
-    explicit RangeError(const std::string& msg) : RuntimeError(msg) {}
-    explicit RangeError(const char* msg) : RuntimeError(msg) {}
-};
-
-class OverflowError : public RuntimeError {
-public:
-    explicit OverflowError(const std::string& msg) : RuntimeError(msg) {}
-    explicit OverflowError(const char* msg) : RuntimeError(msg) {}
-};
-
-class UnderflowError : public RuntimeError {
-public:
-    explicit UnderflowError(const std::string& msg) : RuntimeError(msg) {}
-    explicit UnderflowError(const char* msg) : RuntimeError(msg) {}
-};
 
 
 
