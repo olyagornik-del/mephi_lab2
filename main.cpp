6a0b2504c138ae7f5368774fa0fd02ec313cb409
olyagornik-del <olya.gornik@gmail.com>
2026-05-07 15:37:26 +0300

    creating main, menu.h, scanValues.h, SequenceOperations.h

--- main.cpp
+++ main.cpp
@@ -1,16 +1,17 @@
-#include <iostream>
+#include "scanValues.h"
+#include "menu.h"
+#include "SequenceOperations.h"
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+    printBanner();
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+    int typeChoice;
+    do { printTypeMenu(); } while (!scanInt(typeChoice) || typeChoice < 1 || typeChoice > 3);
+
+    switch (typeChoice) {
+        case 1: runMenu<int>();    break;
+        case 2: runMenu<double>(); break;
+        case 3: runMenu<bool>();   break;
     }
-
     return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
